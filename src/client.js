9718cda9fea35aed76b624761ea65dca6d46dee1
rexdev-labs <63227295+rexdev-labs@users.noreply.github.com>
2020-04-17 02:13:02 +0700

    update
    Key Google Firebase

--- src/client.js
+++ src/client.js
@@ -7,14 +7,5 @@ sapper.start({
 });
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyDxbsCq2sgdrTLjozWrAlpDESK8qqyA9bY",
-    authDomain: "svelte-js-2581a.firebaseapp.com",
-    databaseURL: "https://svelte-js-2581a.firebaseio.com",
-    projectId: "svelte-js-2581a",
-    storageBucket: "svelte-js-2581a.appspot.com",
-    messagingSenderId: "334670005785",
-    appId: "1:334670005785:web:112248beafa859c10b4c88"
-  };
+//isi sendiri di sini 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
